6442ed50eab91510c2bb62e8ff3d1b9805632a3f
CMonkey <42.codemonkey@gmail.com>
2019-12-05 16:27:22 +0800

    add test sendMail

--- src/test/scala/org/excavator/boot/smtp/test/SmtpTests.scala
+++ src/test/scala/org/excavator/boot/smtp/test/SmtpTests.scala
@@ -1,6 +1,6 @@
 package org.excavator.boot.smtp.test
 
-import org.excavator.boot.smtp.Token
+import org.excavator.boot.smtp.{Smtp, Token}
 import org.junit.jupiter.api.{DisplayName, Test}
 
 class SmtpTests {
@@ -9,5 +9,10 @@ class SmtpTests {
   @DisplayName("test send mail")
   def testSendMail(): Unit = {
     val token = Token("smtp.163.com", 25, "", "")
+    val fromAddress = ""
+    val rcptAddress = ""
+    val content = ""
+    val message = ""
+    Smtp().sendMail(fromAddress, rcptAddress, content, message, token)
   }
 }

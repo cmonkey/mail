940d1f6bd0acdc08697dc66fcf4be165f41da52a
CMonkey <42.codemonkey@gmail.com>
2019-12-06 15:46:37 +0800

    add matchSubjectHead

--- src/main/scala/org/excavator/boot/mail/pop3/Pop3Util.scala
+++ src/main/scala/org/excavator/boot/mail/pop3/Pop3Util.scala
@@ -2,13 +2,15 @@ package org.excavator.boot.mail.pop3
 
 import java.io.{BufferedReader, InputStreamReader, PrintWriter}
 import java.net.Socket
+import java.nio.charset.{Charset, StandardCharsets}
 import java.util
-import java.util.StringTokenizer
+import java.util.{Base64, StringTokenizer}
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.function.Consumer
 
 import com.google.common.collect.Lists
 import org.apache.commons.collections4.CollectionUtils
+import org.apache.commons.lang3.StringUtils
 import org.excavator.boot.mail.entity.MailElemnt
 import org.slf4j.LoggerFactory
 
@@ -45,6 +47,39 @@ class Pop3Util {
         mailElements.forEach(mailElement => {
           val num = mailElement.num
           val messageSize = mailElement.messageSize
+          val subjectConsumer:Consumer[BufferedReader] = (bufferedReader:BufferedReader) => {
+            val loop = new AtomicBoolean(true)
+            while(loop.get) {
+              val subject = bufferedReader.readLine()
+              if(StringUtils.isNotBlank(subject)){
+                if(!".".equals(subject)) {
+                  val splitSubject = subject.split(":")
+                  if(splitSubject.head.equals("Subject")) {
+                    val tail = splitSubject.tail.head
+                    val matchSubjectHeadTupl3 = matchSubjectHead(tail)
+                    if(matchSubjectHeadTupl3._1 && tail.endsWith("?=")){
+                      var subjectClean = tail.substring(matchSubjectHeadTupl3._3, tail.length)
+                      subjectClean = subjectClean.substring(0, subjectClean.length - 2)
+                      if(Charset.forName("gb18030").equals(matchSubjectHeadTupl3._2)){
+                        logger.info("top subject  num = [{}], subject = [{}]", num, new String(subjectClean.getBytes(matchSubjectHeadTupl3._2)))
+                      }else {
+                        val bytes = Base64.getDecoder.decode(subjectClean)
+                        val parseSubject = new String(bytes, matchSubjectHeadTupl3._2)
+                        logger.info("top subject num = [{}], subject = [{}]", num, parseSubject)
+                      }
+                    }else{
+                      logger.info("top subject num = [{}], subject = [{}]", num, tail)
+                    }
+                  }else{
+                    logger.info("top subject num = [{}], subject = [{}]", num, subject)
+                  }
+                }
+              }else{
+                loop.set(false)
+              }
+            }
+          }
+          getSubject(printWriter, bufferedReader, num , subjectConsumer)
         })
       }
 
@@ -53,6 +88,22 @@ class Pop3Util {
       printWriter.close()
       socket.close()
     }
+  }
+
+  private def matchSubjectHead(subject: String):Tuple3[Boolean, Charset, Int] = {
+    var tuple3: Tuple3[Boolean, Charset, Int]  = Tuple3(false, StandardCharsets.UTF_8, 11)
+    if(subject.startsWith(" =?utf-8?B?"))  {
+      tuple3 = Tuple3(true, StandardCharsets.UTF_8, 11)
+    }else if(subject.startsWith(" =?UTF-8?B?")){
+      tuple3 = Tuple3(true, StandardCharsets.UTF_8, 11)
+    }else if(subject.startsWith(" =?GBK?B?")){
+      tuple3 = Tuple3(true, Charset.forName("gbk"), 9)
+    }else if(subject.startsWith(" =?gb2312?B?")){
+      tuple3 = Tuple3(true, Charset.forName("gb2312"), 12)
+    }else if (subject.startsWith(" =?GB18030?Q?")){
+      tuple3 = Tuple3(true, Charset.forName("gb18030"), 13)
+    }
+    tuple3
   }
 
   private def getMailElements(bufferedReader: BufferedReader) = {
@@ -68,7 +119,7 @@ class Pop3Util {
             val stringTokenizer = new StringTokenizer(elem)
             val num = Integer.parseInt(stringTokenizer.nextToken())
             val messageSize = Integer.parseInt(stringTokenizer.nextToken())
-            logger.info("list info = [{}] by num = [{}] messageSize = [{}]", Array(elem, num, messageSize))
+            logger.info("list info by num = [{}] messageSize = [{}]", num, messageSize)
 
             val mailElemnt = MailElemnt(num, messageSize)
             mailElements.add(mailElemnt)
@@ -83,8 +134,8 @@ class Pop3Util {
     mailElements
   }
 
-  def getSubect(printWriter: PrintWriter,bufferedReader: BufferedReader, num: Int, consumer: Consumer[BufferedReader]) = {
-    printWriter.println("top " + num)
+  def getSubject(printWriter: PrintWriter,bufferedReader: BufferedReader, num: Int, consumer: Consumer[BufferedReader]) = {
+    printWriter.println("top " + num + " 0")
     consumer.accept(bufferedReader)
   }
 }

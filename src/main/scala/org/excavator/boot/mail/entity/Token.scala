b9d193af258ae787761f6d6eed67b3d234419ac8
CMonkey <42.codemonkey@gmail.com>
2019-12-06 11:15:30 +0800

    change Token

--- src/main/scala/org/excavator/boot/mail/entity/Token.scala
+++ src/main/scala/org/excavator/boot/mail/entity/Token.scala
@@ -1,5 +1,13 @@
 package org.excavator.boot.mail.entity
 
-class Token {
+import java.util.Base64
 
+import scala.beans.BeanProperty
+
+case class Token(@BeanProperty address:String,
+                 @BeanProperty port:Int,
+                 @BeanProperty var userName: String,
+                 @BeanProperty var password: String){
+  userName = Base64.getEncoder.encodeToString(userName.getBytes)
+  password = Base64.getEncoder.encodeToString(password.getBytes())
 }

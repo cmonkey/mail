f1e7891703cd11f4ec817a7b4b54aab386a51a5a
CMonkey <42.codemonkey@gmail.com>
2019-12-06 16:31:26 +0800

    add Pop3UtilTests

--- src/test/scala/org/excavator/boot/mail/pop3/test/Pop3UtilTests.scala
+++ src/test/scala/org/excavator/boot/mail/pop3/test/Pop3UtilTests.scala
@@ -1,5 +1,18 @@
 package org.excavator.boot.mail.pop3.test
 
+import org.excavator.boot.mail.pop3.Pop3Util
+import org.junit.jupiter.api.{DisplayName, Test}
+
 class Pop3UtilTests {
+
+  @Test
+  @DisplayName("test retriverMail")
+  def testRetriverMail() = {
+    val pop3Address = "pop.163.com"
+    val port = 110
+    val userName = ""
+    val password = ""
+    Pop3Util().retriverMail(pop3Address, port, userName, password)
+  }
 
 }

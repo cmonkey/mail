5537c18beedcd31d17b23bfafc33e425cc1f034f
CMonkey <42.codemonkey@gmail.com>
2019-12-06 11:13:32 +0800

    add Pop3Util.retriverMail function

--- src/main/scala/org/excavator/boot/mail/pop3/Pop3Util.scala
+++ src/main/scala/org/excavator/boot/mail/pop3/Pop3Util.scala
@@ -1,5 +1,29 @@
 package org.excavator.boot.mail.pop3
 
-class Pop3Util {
+import java.io.{BufferedInputStream, BufferedReader, PrintWriter}
+import java.net.Socket
 
+import org.slf4j.LoggerFactory
+
+class Pop3Util {
+  val logger = LoggerFactory.getLogger(classOf[Pop3Util])
+
+  def retriverMail() = {
+
+    var socket: Socket = null
+    var printWriter: PrintWriter = null
+    var bufferedReader: BufferedReader = null
+
+    try{
+
+    }finally{
+      bufferedReader.close()
+      printWriter.close()
+      socket.close()
+    }
+  }
+}
+
+object Pop3Util{
+  def apply(): Pop3Util = new Pop3Util()
 }
